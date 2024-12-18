dc7250f1c66edeec6cc21efd75da0552fb703a04
Cookie4686 <cookieandcake1105@gmail.com>
2024-12-18 16:36:18 +0700

    specify theme options

--- app/lib/theme.ts
+++ app/lib/theme.ts
@@ -5,5 +5,5 @@ import { cookies } from "next/headers";
 export async function setTheme() {
   const cookieStore = await cookies();
   const theme = cookieStore.get('theme');
-  cookieStore.set({ name: 'theme', value: theme && theme.value == 'dark' ? 'light' : 'dark' });
+  cookieStore.set({ name: 'theme', value: theme && theme.value == 'dark' ? 'light' : 'dark', maxAge: 259200, priority: 'low', sameSite: 'lax' });
 }
